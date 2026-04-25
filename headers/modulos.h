5cb9b534b7df53b2c1917a4cc42bca69572b6a60
gatotabaco <matheus.fsousa11@gmail.com>
2026-04-25 19:14:00 -0300

    adiciona mapeamento dos modulos

---
+++ headers/modulos.h
@@ -0,0 +1,25 @@
+/*Nesse arquivo estão declaradas as constantes conforme o enunciado definiu
+
+No modelo 1 o acionamento é direto e cada pino controla um LED independente
+modelo 2 é código de 3 bits pela raspberry
+
+*/
+
+// Modelo 1, cruzamento 1
+
+#define M1_VERDE 17
+#define M1_AMARELO 18
+#define M1_VERMELHO 23
+
+// Modelo 2, cruzamento 2
+
+#define M2_BIT0 24
+#define M2_BIT1 8
+#define M2_BIT2 7
+
+//Botões de pedestres
+
+#define BOTAO_M1_P 1
+#define BOTAO_M1_C 12
+#define BOTAO_M2_P 25
+#define BOTAO_M2_C 22
